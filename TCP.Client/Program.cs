86f50b906bf7f1ff94e61f26f9cba8b6740ea59b
Guilherme Souza <guilhermesouzae21@gmail.com>
2024-04-13 10:49:27 -0300

    Initial commit

---
+++ TCP.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+TcpClient client = new TcpClient("localhost", 9999);
+
+// Get the stream used for reading and writing.
+NetworkStream stream = client.GetStream();
+
+// Send the message to the connected server. 
+string message = "Hello Server!\n";
+byte[] data = Encoding.ASCII.GetBytes(message);
+Console.WriteLine(data.Length);
+stream.Write(data, 0, data.Length);
+
+Console.WriteLine("Sent: {0}", message);
+
+// Buffer to store the response bytes.
+data = new byte[256];
+
+// String to store the response ASCII representation.
+string responseData = string.Empty;
+
+// Read the first batch of the server response bytes.
+int bytes = stream.Read(data, 0, data.Length);
+responseData = Encoding.ASCII.GetString(data, 0, bytes);
+Console.WriteLine("Received: {0}", responseData);
+
+// Close everything.
+stream.Close();
+client.Close();
---
+++ TCP.Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+
+var server = new TcpListener(IPAddress.Any, 9999);
+server.Start();
+
+
+while (true)
+{
+    // wait for client connection
+    using TcpClient newClient = server.AcceptTcpClient();
+    Console.WriteLine("New Client = {0}", newClient.Connected);
+
+    // sets two streams
+    var sReader = new StreamReader(newClient.GetStream(), Encoding.ASCII);
+    var sWriter = new StreamWriter(newClient.GetStream(), Encoding.ASCII);
+
+    string sData = null;
+    Console.WriteLine("Client connected: {0}", newClient.Connected);
+    char[] buffer = new char[1024];
+
+    if (newClient.Connected)
+    {
+        Console.WriteLine($"newClient = {newClient.Connected}");
+        // reads from stream
+        sData = sReader.ReadLine();
+        Console.WriteLine($"Data = {sData}");
+
+        // shows content on the console.
+        Console.WriteLine("Client says: " + sData);
+
+        // to write something back.
+        sWriter.WriteLine("OK!");
+        sWriter.Flush();
+
+        newClient.Close();
+    }
+}

5d8a7286f7d120153374f42d9a9ad9829ee121ee
Guilherme Souza Araújo <guilherme.souza@dtidigital.com.br>
2024-04-15 20:40:35 -0300

    feat: custom tcp package format

--- TCP.Client/Program.cs
+++ TCP.Client/Program.cs
@@ -3,72 +3,21 @@ using System.Text;
 
 var client = new TcpClient("localhost", 9999);
 
-// Get the stream used for reading and writing.
 var stream = client.GetStream();
 
-// Buffer to store the response bytes.
-var data = new byte[256];
+var version = new byte[1];
+var decompressionMethod = new byte[5];
+var dataSize = new byte[1];
 
 string responseData = string.Empty;
 
-var screen = new string[5, 5];
-for (int i = 0; i < 5; i++)
+while (stream.CanRead)
 {
-    for (int j = 0; j < 5; j++)
-    {
-        screen[i, j] = "X ";
-    }
-}
-Console.WriteLine("-----------------");
-DisplayScreen();
+    await stream.ReadExactlyAsync(version);
+    await stream.ReadExactlyAsync(decompressionMethod);
+    await stream.ReadExactlyAsync(dataSize);
 
-while (stream.CanRead) // Keep reading until no more data
-{
-    int bytes = await stream.ReadAsync(data);
-    responseData += Encoding.ASCII.GetString(data, 0, bytes);
-
-    // Process all complete messages
-    while (responseData.Contains(';'))
-    {
-        var messageEnd = responseData.IndexOf(';');
-        var message = responseData.Substring(0, messageEnd);
-        // remove previous "0"
-
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                if (screen[i, j] == "O ")
-                {
-                    screen[i, j] = "X ";
-                }
-            }
-        }
-
-        var x = int.Parse(message.Split('/')[0]);
-        var y = int.Parse(message.Split('/')[1]);
-        screen[x, y] = "O ";
-        Console.WriteLine("-----------------");
-
-        DisplayScreen();
-
-        responseData = responseData.Substring(messageEnd + 1);
-    }
-}
-
-stream.Close();
-client.Close();
-
-
-void DisplayScreen()
-{
-    Console.Clear();
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            Console.Write(screen[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
+    Console.WriteLine($"Version = {version[0]}");
+    Console.WriteLine($"Decompression method = {Encoding.ASCII.GetString(decompressionMethod)}");
+    Console.WriteLine($"Data size = {dataSize[0]}");
 }
--- TCP.Server/Program.cs
+++ TCP.Server/Program.cs
@@ -1,35 +1,66 @@
-﻿using System.Net.Sockets;
+﻿using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
-
-
-var server = new TcpListener(IPAddress.Any, 9999);
-server.Start();
-Console.WriteLine("Server Running!");
-
-while (true)
+class Program
 {
-    TcpClient newClient = await server.AcceptTcpClientAsync();
+    const int VERSION = 69;
+    const string DECOMPRESSION_METHOD = "GSA33";
+    const byte VERSION_SIZE = 1;
+    const byte DECOMPRESSION_METHOD_SIZE = 5;
 
-    var thread = new Thread(new ParameterizedThreadStart(SendMessage));
-
-    thread.Start(newClient);
-}
-
-async void SendMessage(object c)
-{
-    var client = (TcpClient)c;
-
-    var stream = client.GetStream();
-
-    // Console.WriteLine("Data length = {0}", messageData.Length);
-
-    var random = new Random();
-    for (int x = 0; x < 150; x++)
+    static void Main(string[] args)
     {
-        var messageData = Encoding.ASCII.GetBytes($"{random.Next(0, 5)}/{random.Next(0, 5)};");
-        await stream.WriteAsync(messageData);
-        await Task.Delay(1250);
+        var server = new TcpListener(IPAddress.Any, 9999);
+        server.Start();
+        Console.WriteLine("Server Running!");
+
+        while (true)
+        {
+            TcpClient newClient = server.AcceptTcpClient();
+
+            var thread = new Thread(new ParameterizedThreadStart(SendMessage));
+
+            thread.Start(newClient);
+        }
+    }
+
+    static void SendMessage(object c)
+    {
+        var random = new Random();
+        var client = (TcpClient)c;
+        var stream = client.GetStream();
+
+        byte[] decompressionBytes = Encoding.ASCII.GetBytes(DECOMPRESSION_METHOD);
+
+        for (int x = 0; x < 1; x++)
+        {
+            // var myData = new byte[16];
+            var data = Encoding.ASCII.GetBytes("Oie");
+            var dataSize = (byte)data.Length;
+
+
+            // if(dataSize <= 16){
+            // byte[] messageData = new byte[VERSION_SIZE + DECOMPRESSION_METHOD_SIZE + dataSize + 16];
+            byte[] messageData = new byte[VERSION_SIZE + DECOMPRESSION_METHOD_SIZE + dataSize];
+
+            messageData[0] = VERSION;
+
+            Array.Copy(decompressionBytes, 0, messageData, 1, 5);
+
+            messageData[6] = dataSize;
+
+            // Array.Copy(data, 0, messageData, 7, dataSize);
+
+            // message array - contains my custom package
+            // VERSION | DECOMPRESSION_METHOD | DATA_SIZE | DATA
+            // 0 write from the start of the message array
+            // messageData.Length write the entire message array
+            stream.Write(messageData, 0, messageData.Length);
+
+            Thread.Sleep(1250);
+        }
     }
 }
